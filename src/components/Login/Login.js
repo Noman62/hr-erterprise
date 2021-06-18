963ee6edf954756abcce48cab55db56e8e3740c9
Noman62 <noman.ice.nstu@gmail.com>
2021-06-18 12:46:30 -0700

    design complete

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,7 +4,7 @@ import "firebase/auth";
 import firebaseConfig from './firebase.config';
 import { UserContext } from '../../App';
 import { useHistory, useLocation } from 'react-router';
-import { SocialIcon } from 'react-social-icons';
+import './Login.css';
 
 if (firebase.apps.length === 0) {
     firebase.initializeApp(firebaseConfig);
@@ -57,32 +57,17 @@ const Login = () => {
     }
 
     return (
-        <div className=" shadow p-3 mb-5 bg-body rounded" style={{ width: '350px', height: '480px', textAlign: 'center', marginLeft: '38%', marginTop: '5%' }}>
+        <div  >
             <div className="signin-form">
                 <form action="/examples/actions/confirmation.php" method="post">
                     <h2>Sign in</h2>
                     <p className="hint-text">Sign in with your social media account</p>
-                    <div className="social-btn mb-3  text-center">
-                        <a href="#" className="btn btn-primary btn-lg mr-3 rounded-circle" title="Facebook" onClick={handleFbSignIn}><i className="fa fa-facebook"></i></a>
-                        <a href="#" className="btn btn-danger btn-lg mr-3 rounded-circle" title="Google" onClick={handleGoogleSignIn}><i className="fa fa-google"></i></a>
-                        <a href="#" class="btn btn-info btn-lg rounded-circle" title="Twitter"><i class="fa fa-twitter"></i></a>
-
+                    <div className="d-flex justify-content-center align-items-center ">
+                        <a style={{backgroundColor:'#507cc0',color:'#fff'}} href="#" className="btn border border-2 rounded-circle btn-lg mr-2 " title="Facebook"><i className="fa fa-facebook"></i></a>
+                        <a style={{backgroundColor:'#64ccf1',color:'#fff'}} href="#" className="btn border border-2 rounded-circle  btn-lg mr-2" title="Twitter"><i className="fa fa-twitter"></i></a>
+                        <a style={{backgroundColor:'#df4930',color:'#fff'}} href="#" className="btn border border-2 rounded-circle btn-lg" title="Google"><i className="fa fa-google"></i></a>
                     </div>
-                    <div style={{ margin: '50px 0 15px', textAlign: 'center', borderTop: '1px solid #e0e0e0' }} ><b style={{
-                        padding: '0 10px',
-                        width: '40px',
-                        height: '40px',
-                        fontSize: '16px',
-                        textAlign: 'center',
-                        lineHeight: '40px',
-                        background: ' #fff',
-                        display: 'inline-block',
-                        border: '1px solid #e0e0e0',
-                        borderRadius: '50%',
-                        position: 'relative',
-                        top: '-22px',
-                        zIndex: '1'
-                    }}>or</b></div>
+                    <div className="or-seperator"><b>or</b></div>
                     <div className="form-group">
                         <input type="text" className="form-control input-lg" name="username" placeholder="Username" required="required" />
                     </div>
@@ -90,10 +75,10 @@ const Login = () => {
                         <input type="password" className="form-control input-lg" name="password" placeholder="Password" required="required" />
                     </div>
                     <div className="form-group">
-                        <button type="submit" className="btn btn-success btn-lg btn-block signin-btn rounded-pill">Sign in</button>
+                        <button type="submit" className="btn btn-success btn-lg btn-block signin-btn">Sign in</button>
                     </div>
                     <div className="text-center small"><a href="#">Forgot Your password?</a></div>
-                    <div class="text-center small">Don't have an account? <a href="/signUp">Sign up</a></div>
+                    <div className="text-center small">Don't have an account? <a href="/signUp">Sign up</a></div>
                 </form>
                 
             </div>
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,34 +1,39 @@
-// import React from 'react';
-// import './SignUp.css';
+import React from 'react';
+import '../Login/Login.css';
+const SignUp = () => {
+    return (
+        <div>
+            <div className="signin-form">
+                <form action="/examples/actions/confirmation.php" method="post">
+                    <h2>Create An Account</h2>
+                    <p className="hint-text">Sign up with your social media account or email address</p>
+                    <div className="d-flex justify-content-center align-items-center ">
+                        <a style={{ backgroundColor: '#507cc0', color: '#fff' }} href="#" className="btn border border-2 rounded-circle btn-lg mr-2 " title="Facebook"><i className="fa fa-facebook"></i></a>
+                        <a style={{ backgroundColor: '#64ccf1', color: '#fff' }} href="#" className="btn border border-2 rounded-circle  btn-lg mr-2" title="Twitter"><i className="fa fa-twitter"></i></a>
+                        <a style={{ backgroundColor: '#df4930', color: '#fff' }} href="#" className="btn border border-2 rounded-circle btn-lg" title="Google"><i className="fa fa-google"></i></a>
+                    </div>
+                    <div className="or-seperator"><b>or</b></div>
+                    <div className="form-group">
+                        <input type="text" className="form-control input-lg" name="username" placeholder="Username" required="required" />
+                    </div>
+                    <div className="form-group">
+                    <input type="email" class="form-control input-lg" name="email" placeholder="Email Address" required="required"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="password" className="form-control input-lg" name="password" placeholder="Password" required="required" />
+                    </div>
+                    <div className="form-group">
+                        <input type="password" className="form-control input-lg" name="confirm_password" placeholder="Confirm Password" required="required" />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-success btn-lg btn-block signin-btn">Sign Up</button>
+                    </div>
+                    <div className="text-center small">Already have an account? <a href="/login">Login here</a></div>
+                </form>
 
-// const SignUp = () => {
-//     return (
-//         <div className=" shadow p-3 mb-5 bg-body rounded" style={{ width: '350px', height: '480px', textAlign: 'center', marginLeft: '38%', marginTop: '5%' }}>
-//             <div class="signup-form">
-//                 <form action="/examples/actions/confirmation.php" method="post">
-//                     <h2>Create an Account</h2>
-//                     <p class="hint-text">Sign up with your social media account or email address</p>
-                        
-//                     <div class="form-group">
-//                         <input type="text" class="form-control input-lg" name="username" placeholder="Username" required="required" />
-//                     </div>
-//                     <div class="form-group">
-//                         <input type="email" class="form-control input-lg" name="email" placeholder="Email Address" required="required" />
-//                     </div>
-//                     <div class="form-group">
-//                         <input type="password" class="form-control input-lg" name="password" placeholder="Password" required="required" />
-//                     </div>
-//                     <div class="form-group">
-//                         <input type="password" class="form-control input-lg" name="confirm_password" placeholder="Confirm Password" required="required" />
-//                     </div>
-//                     <div class="form-group">
-//                         <button type="submit" class="btn btn-success btn-lg btn-block signup-btn">Sign Up</button>
-//                     </div>
-//                 </form>
-//                 <div class="text-center">Already have an account? <a href="/login">Login here</a></div>
-//             </div>
-//         </div>
-//     );
-// };
+            </div>
+        </div>
+    );
+};
 
-// export default SignUp;
+export default SignUp;
